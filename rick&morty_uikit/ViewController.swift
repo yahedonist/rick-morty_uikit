dc3151f7196ba28b425e0ff2a03cd26ae2a16bd5
yahedonist <yatopolskov@gmail.com>
2023-11-01 13:30:29 +0300

    Initial Commit

---
+++ rick&morty_uikit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rick&morty_uikit
+//
+//  Created by Nikita on 01.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
